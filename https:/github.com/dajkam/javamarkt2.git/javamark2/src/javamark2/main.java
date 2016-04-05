3819db195493984bc209a71adfcc5b0db508ed1f
s13033 <s13033@lab5l-2.pjwstk.edu.pl>
2016-04-05 12:57:08 +0200

    ddddd

--- https:/github.com/dajkam/javamarkt2.git/javamark2/src/javamark2/main.java
+++ https:/github.com/dajkam/javamarkt2.git/javamark2/src/javamark2/main.java
@@ -2,7 +2,9 @@ package javamark2;
 
 public class main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
+		
+		int nowy;
 		// TODO Auto-generated method stub
 
 	}
